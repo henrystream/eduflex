c82b05194cb6974485c6084411f09538af9426ea
henrystream <henrystreamhenry@gmail.com>
2026-08-24 03:12:56 +0400

    Quick update: The system cannot accept the date entered. Enter the new date: (mm-dd-yy)

--- reporting-service/internal/config/config.go
+++ reporting-service/internal/config/config.go
@@ -13,10 +13,10 @@ type Config struct {
 
 func Load() Config {
 	return Config{
-		LedgerURL:    getEnv("LEDGER_SERVICE_URL", "http://ledger-service:8084"),
-		FinancingURL: getEnv("FINANCING_SERVICE_URL", "http://financing-service:8082"),
-		LoanURL:      getEnv("LOAN_SERVICE_URL", "http://loan-service:8083"),
-		StudentURL:   getEnv("STUDENT_SERVICE_URL", "http://student-service:8081"),
+		LedgerURL:    getEnv("LEDGER_SERVICE_URL", "http://ledger-service:8080"),
+		FinancingURL: getEnv("FINANCING_SERVICE_URL", "http://financing-service:8080"),
+		LoanURL:      getEnv("LOAN_SERVICE_URL", "http://loan-service:8080"),
+		StudentURL:   getEnv("STUDENT_SERVICE_URL", "http://student-service:8080"),
 		SchoolURL:    getEnv("SCHOOL_SERVICE_URL", "http://school-service:8080"),
 		Port:         getEnv("PORT", "8085"),
 	}
